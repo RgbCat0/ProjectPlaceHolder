2abe0469522e286ea0fe42da991bb86f0d3cfc48
Nqrdie <rrvanleeuwen@kpnmail.nl>
2025-06-13 10:53:07 +0200

    Settings menu

--- Assets/_Scripts/Settings.cs
+++ Assets/_Scripts/Settings.cs
@@ -6,11 +6,13 @@ public class Settings : MonoBehaviour
     [SerializeField] private GameObject mainPanel;
     [SerializeField] private GameObject keybindsPanel;
     [SerializeField] private GameObject videoPanel;
+    [SerializeField] private GameObject closePanel;
     public void SwitchTabs(GameObject newPanel)
     {
        newPanel.SetActive(true);
        if(newPanel == mainPanel) {keybindsPanel.SetActive(false); videoPanel.SetActive(false);}
        if(newPanel == keybindsPanel) {mainPanel.SetActive(false); videoPanel.SetActive(false);}
        if(newPanel == videoPanel) {mainPanel.SetActive(false); keybindsPanel.SetActive(false);}
+       if(newPanel == closePanel) {mainPanel.SetActive(false); keybindsPanel.SetActive(false); videoPanel.SetActive(false);} 
     }
 }
