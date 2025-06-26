7ca274e8b728b6b566010c98a5eff2cc9189d53e
Nqrdie <rrvanleeuwen@kpnmail.nl>
2025-06-26 15:51:18 +0200

    fix basic attack height

---
+++ Assets/_Scripts/FixBasicAttack.cs
@@ -0,0 +1,10 @@
+using System;
+using UnityEngine;
+
+public class FixBasicAttack : MonoBehaviour
+{
+    private void Update()
+    {
+        transform.position = new Vector3(0, 0, -1.5f);
+    }
+}
