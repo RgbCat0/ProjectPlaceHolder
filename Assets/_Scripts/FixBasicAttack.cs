e9cce54ed1203c688838bc22de943280f51a7fa1
Nqrdie <rrvanleeuwen@kpnmail.nl>
2025-06-27 09:18:10 +0200

    basic attack fix

--- Assets/_Scripts/FixBasicAttack.cs
+++ Assets/_Scripts/FixBasicAttack.cs
@@ -5,6 +5,6 @@ public class FixBasicAttack : MonoBehaviour
 {
     private void Update()
     {
-        transform.position = new Vector3(0, 0, -1.5f);
+        transform.position = new Vector3(transform.position.x, 1, transform.position.z);
     }
 }
